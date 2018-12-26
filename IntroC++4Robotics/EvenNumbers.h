50e1579079d13fcd0ba8dc24c4c79fab401c2ce9
Oushesh Haradhun <oushesh@gmail.com>
2018-12-26 13:07:54 +0100

    Fixed Folder Structure

--- IntroC++4Robotics/EvenNumbers.h
+++
@@ -1,7 +0,0 @@
-//
-// Created by Oushesh on 26/12/2018.
-//
-#ifndef INTROC_4ROBOTICS_EVENNUMBERS_H  // To make sure you don't declare the function more than once by including the header multiple times.
-#define INTROC_4ROBOTICS_EVENNUMBERS_H
-void FindEven(int array []);
-#endif //INTROC_4ROBOTICS_EVENNUMBERS_H
